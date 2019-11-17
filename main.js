afaab7391d7f57e25e532e99fb3221ff386b890e
Yoni <yonish3@gmail.com>
2019-11-17 11:23:50 +0200

    new

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+
+let checkForm = function(){
+    let name = document.getElementById("nameOf")
+    let salary = document.getElementById("salary").value
+    let birthday = document.getElementById("birthday").value
+    let phone = document.getElementById("phone").value
+    document.getElementById("result").innerHTML =""
+
+    if  (name.value.length < 2){
+        document.getElementById("result").innerHTML += "Name too short \n" 
+    }
+    if (salary <10000 || salary> 16000) {
+        document.getElementById("result").innerHTML += "Too much/littel salary requisted \n"
+    }
+    if (!birthday) {
+        document.getElementById("result").innerHTML += "what are you 0 years old?? \n"
+        
+    }
+    if (phone.length!=10) {
+        document.getElementById("result").innerHTML += "Check phonenumber \n"
+        
+    }
+}
